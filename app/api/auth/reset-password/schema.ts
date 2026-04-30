e1e33b8d862c1bae7466d903bd99ad18e7e3d4bd
Manuel Haas <manuelhaas99@gmail.com>
2026-04-30 17:28:38 -0600

    Adding validation schemes for auth

---
+++ app/api/auth/reset-password/schema.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+export const resetPasswordSchema = z.object({
+    email: z.string().email({ message: 'Invalid email address'}),
+    newPassword: z.string().min(8).max(100),
+    confirmPassword: z.string().min(8).max(100),
+    token: z.string().min(1, { message: 'Token is required' }),
+    
+})
+.refine((data) => data.newPassword === data.confirmPassword, {
+    message: 'Las contraseñas no coinciden',
+    path: ['confirmPassword']
+});
