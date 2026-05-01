721f5aa3de2fe05801a5d11c6f5c97f8d2adb0e7
Manuel Haas <manuelhaas99@gmail.com>
2026-05-01 13:38:42 -0600

    Adding validation scheme for clients

---
+++ app/api/clients/schema.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+export const clientSchema = z.object({
+    name: z.string().trim().min(2, { message: 'Name must be at least 2 characters long' }).max(100),
+    email: z.string().email( { message: 'Invalid email address' } ),
+    phone: z.string().regex(/^\+?[1-9]\d{1,14}$/, { message: 'Invalid phone number format' }),
+})
